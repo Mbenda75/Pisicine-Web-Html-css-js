531482b41f897a5d20622b08f3eb26e5a469851f
Mohamed BENDAOUD <benmoham@e2r13p11.42.fr>
2022-06-20 19:23:59 +0200

    need to end cell1.4

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+if [ -z "$1" ]
+then
+        echo "No arguments supplied";
+fi
+
+if [ -n "$1" ]
+then
+        echo $1
+fi
+if [ -n "$2" ]
+then
+        echo $2
+fi
+if [ -n "$3" ]
+then
+        echo $3
+fi
+if [ -n "$4" ]
+then
+        echo $4
+fi
+if [ -n "$5" ]
+then
+        echo $5
+fi
